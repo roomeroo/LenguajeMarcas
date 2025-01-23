6562e48637ca15106a0ec7181a0d35d3940f0eaf
Adrian romero maldonado <roomeroo05@gmail.com>
2025-01-23 15:24:53 +0100

    redistribucion

---
+++ javascript/ejercicios/ejercicios.js
@@ -0,0 +1,17 @@
+console.log("Ejercicio 1");
+
+const precio = 200;
+const iva = 0.21;
+console.log("El total es: " + (precio + precio * iva));
+
+/*------------------------------------------------------------*/
+console.log("Ejercicio 2");
+
+
+const lado = 40;
+const area = lado ** 2;
+const perimetro = lado * 4;
+console.log("El area es: " + area + '\n' + "El perimetro es: " + perimetro);
+
+/*------------------------------------------------------------*/
+console.log("Ejercicio ");
